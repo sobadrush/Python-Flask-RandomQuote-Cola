886463cadd81759a4a6032c31f07cd5afc9b22e1
Roger Lo <sobadrush@icloud.com>
2025-04-26 22:41:06 +0800

    新增跳轉到 add.html 的路由 & 按鈕

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 @app.route("/")
 def aa():
     quotes = [
-        { 'author': '漢堡老師', 'quote': "上課要專心" },
-        { 'author': '唐納·川普', 'quote': "You don't have cards." },
-        { 'author': '江戶川·柯南', 'quote': "真相只有一個" },
-        { 'author': '和田教授', 'quote': "休息是為了走更長遠的路" },
-        { 'author': '廣志', 'quote': "走更遠的路, 是為了休息" },
-        { 'author': '館長', 'quote': "暴力不能解決問題，但能解決你" }
+        {"author": "漢堡老師", "quote": "上課要專心"},
+        {"author": "唐納·川普", "quote": "You don't have cards."},
+        {"author": "江戶川·柯南", "quote": "真相只有一個"},
+        {"author": "和田教授", "quote": "休息是為了走更長遠的路"},
+        {"author": "廣志", "quote": "走更遠的路, 是為了休息"},
+        {"author": "館長", "quote": "暴力不能解決問題，但能解決你"},
     ]
     print(" === 有人連線 === ")
     return render_template("index.html", quoteX=random.choice(quotes))
 
+@app.route("/addQuote")
+def addQuote():
+  print(" === 進入 addQuote === ")
+  return render_template('add.html')
 
 # app.run()
 if __name__ == "__main__":
-  app.run(debug=True, port=8787)
+    app.run(debug=True, port=8787)
