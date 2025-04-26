fcdb7dc13200b0a3833d922f23476395f0d2f410
Roger Lo <sobadrush@icloud.com>
2025-04-26 23:11:31 +0800

    重構 app.py 1. 將名言列表移至全域變數 2. 調整路由函式名稱，並在 addQuote 中新增表單驗證及重定向功能 redirect

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import random
 
 app = Flask(__name__)
 
+quotes = [
+    {"author": "漢堡老師", "quote": "上課要專心"},
+    {"author": "唐納·川普", "quote": "You don't have cards."},
+    {"author": "江戶川·柯南", "quote": "真相只有一個"},
+    {"author": "和田教授", "quote": "休息是為了走更長遠的路"},
+    {"author": "廣志", "quote": "走更遠的路, 是為了休息"},
+    {"author": "館長", "quote": "暴力不能解決問題，但能解決你"},
+]
 
 @app.route("/")
-def aa():
-    quotes = [
-        {"author": "漢堡老師", "quote": "上課要專心"},
-        {"author": "唐納·川普", "quote": "You don't have cards."},
-        {"author": "江戶川·柯南", "quote": "真相只有一個"},
-        {"author": "和田教授", "quote": "休息是為了走更長遠的路"},
-        {"author": "廣志", "quote": "走更遠的路, 是為了休息"},
-        {"author": "館長", "quote": "暴力不能解決問題，但能解決你"},
-    ]
+def index():
     print(" === 有人連線 === ")
+    print(f">>> quotes : {quotes}")
     return render_template("index.html", quoteX=random.choice(quotes))
 
 @app.route("/addQuote", methods=['GET', 'POST'])
 def addQuote():
-  print(" === 進入 addQuote === ")
-  qouteText = request.form["qouteText"]
-  qouteAuthor = request.form.get("qouteAuthor")
-  print(f">>> qouteText: {qouteText}, qouteAuthor: {qouteAuthor}")
+  if request.method == 'POST':
+    print(" === 進入 addQuote === ")
+    
+    # 從表單獲取新的名言和作者
+    qouteText = request.form["qouteText"]
+    qouteAuthor = request.form.get("qouteAuthor")
+    print(f">>> qouteText: {qouteText}, qouteAuthor: {qouteAuthor}")
+    
+    # 確保兩個欄位都不為空
+    if qouteText and qouteAuthor:
+      quotes.append({"quote": qouteText, "author": qouteAuthor})
+      print(f">>> url_for('index') 是 : {url_for('index')}")
+      return redirect(url_for('index'))
+    
   return render_template('add.html')
 
 # app.run()
