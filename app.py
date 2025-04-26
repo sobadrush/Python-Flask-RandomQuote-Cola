6840bb8511ef588a6e982dc9f64f0d9323ab8c2e
Roger Lo <sobadrush@icloud.com>
2025-04-26 22:25:27 +0800

    增加 quote-container

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def aa():
     quotes = [
-      '早起的鳥兒有蟲吃',
-      '學海無涯, 回頭是岸',
-      '休息是為了走更長遠的路',
-      '走更遠的路, 是為了休息的更久',
-      '暴力不能解決問題，但能解決你'
+        "上課要專心",
+        "學海無涯, 回頭是岸",
+        "休息是為了走更長遠的路",
+        "走更遠的路, 是為了休息的更久",
+        "暴力不能解決問題，但能解決你",
     ]
     print(" === 有人連線 === ")
-    return render_template("index.html",quote=random.choice(quotes))
+    return render_template("index.html", quote=random.choice(quotes))
 
-app.run()
+
+# app.run()
+if __name__ == "__main__":
+  app.run(debug=True, port=8787)
