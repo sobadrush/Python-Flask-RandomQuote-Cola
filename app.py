f0f4c1d46841c49bc61fd784dcd3ff6f2c6649a6
Roger Lo <sobadrush@icloud.com>
2025-04-26 22:35:51 +0800

    更新名言格式，將名言與作者分開顯示，並調整變數名稱以符合新結構

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def aa():
     quotes = [
-        "上課要專心",
-        "學海無涯, 回頭是岸",
-        "休息是為了走更長遠的路",
-        "走更遠的路, 是為了休息的更久",
-        "暴力不能解決問題，但能解決你",
+        { 'author': '漢堡老師', 'quote': "上課要專心" },
+        { 'author': '唐納·川普', 'quote': "You don't have cards." },
+        { 'author': '江戶川·柯南', 'quote': "真相只有一個" },
+        { 'author': '和田教授', 'quote': "休息是為了走更長遠的路" },
+        { 'author': '廣志', 'quote': "走更遠的路, 是為了休息" },
+        { 'author': '館長', 'quote': "暴力不能解決問題，但能解決你" }
     ]
     print(" === 有人連線 === ")
-    return render_template("index.html", quote=random.choice(quotes))
+    return render_template("index.html", quoteX=random.choice(quotes))
 
 
 # app.run()
